f22d22b6f3066045573dd4ed72e41db42dde9736
SandeepOmanakuttan01 <120357000+SandeepOmanakuttan01@users.noreply.github.com>
2024-11-29 15:27:10 +0530

    prompt change.py

--- chat.py
+++ chat.py
@@ -275,11 +275,11 @@ def handle_chat_interaction(prompt):
     category_prompts = {
     "Bra": """
    i want answers related to shyaway.com alone
-   category = Bra
+   category : Bra
    offers=buy-3-for-1199,buy-2-for-1299,flat-20%-off,buy-3-for-899,flat-50%-off,flat-40%-off,new-arrival
    color-family=Grey,Black,White,Skin,Brown,Yellow,Orange,Pink,Red,Green,Blue,Purple,Prints
    fabric=Nylon,Viscose-Spandex,Nylon-Polyester-Spandex,Cotton,Cotton-Spandex,Lace,Mesh,Modal,Polyester-Spandex,Polycotton-Spandex,Satin
-   bra-type=Beginners,Bralette,Cami,Everyday,Fashion-Fancy,Minimiser,Push-Up,T-Shirt
+   bra-type=Beginners,Bralette,Cami,Everyday,Fashion-Fancy,Minimiser,Push-Up,T-Shirt,Nursing
    bra-feature=Backless,Bridal,Casual,Designer,Fancy-Back,Front-Open,Hi-Support,Lacework,Longline,Moulded,No-Sag,Plus-Size,Printed,Sexy,Sleep,Transparent
    bra-coverage=full-coverage,3-4-th-Coverage,Demi-Coverage
    bra-padding=Non-Padded,Padded,Removable-Padding,Lightly-Padded
@@ -288,11 +288,14 @@ def handle_chat_interaction(prompt):
    bra-push-up-level=level-1,level-2,level-3
    bra-closure=back-closure,front-closure,slip-on
    brand=susie,taabu,shyle,shyaway
-   size=30a,30b,30c,30d,30dd,32a,32b,32c,32d,32dd,34a,34b,34c,34d,34dd,36a,36b,36c,36d,36dd,38a,38b,38c,38d,38dd,40a,40b,40c,40d,40dd,42a,42b,42c,42d,42dd,44a,44b,44c,44d,44dd,46a,46b,46c,46d,46dd,48a,48b,48c,48d,48dd,50a,50b,50c,50d,50dd,52a,52b,52c,52d,52dd
+   size=28(D,DD,E),30(A,B,C,D,DD,E),32(A,B,C,D,DD,E,F,G,H,I,J),34(A,B,C,D,DD,E,F,G,H,I,J),36(A,B,C,D,DD,E,F,G,H,I,J),38(A,B,C,D,DD,E,F,G,H,I),40(B,C,D,DD,E,F,G,H,I,J),42(B,C,D,E,F,G),44(B,C,D,E,F,G),46(B,C,D),48(B,C,D),50(B,C,D),52(B,C,D)
    price=0-300,1200-1500,1500-1800,300-600,600-900,900-1200
+   Convert the query into attributes for the Bra category. Match synonyms or contextual words with the listed bra attributes and provide the result in the following format:
+   category: bra, url: https://www.shyaway.com/bra-online/?{attribute=value}
+   Example : category: bra, url: https://www.shyaway.com/bra-online/?color-family=red,green&size=32B
     """,
     "Panty": """
-   category = Panty
+   category : Panty
    offers=buy-3-for-499,buy-3-for-599,flat-20%-off,flat-30,new-arrival
    brand=susie,taabu,shyle,shyaway
    color-family=Grey,Black,White,Skin,Brown,Yellow,Orange,Pink,Red,Green,Blue,Purple,Prints,multicolor
@@ -303,10 +306,13 @@ def handle_chat_interaction(prompt):
    panty-coverage=full-coverage,low-coverage,medium-coverage,no-coverage
    panty-waist-level=high-waist,low-waist,medium-waist
    price=0-300,1200-1500,1500-1800,300-600,600-900,900-1200
+   Convert the query into attributes for the Panty category. Match synonyms or contextual words with the listed panty attributes and provide the result in the following format:
+   category: panty, url: https://www.shyaway.com/panty-online/?{attribute=value}
+   Example : category: panty, url: https://www.shyaway.com/panty-online/?fabric=cotton&size=XL  
     """,
     "lingerie-set":"""
     i want  answers related to shyaway.com alone
-    category = lingerie-set
+    category : lingerie-set
    offers=buy-3-for-499,buy-3-for-599,flat-20%-off,flat-30,new-arrival
    size=38D/XL,40B/XXL,40C/XXL,40D/XXL,32D/S,32 D/DD,34 D/DD,36 D/DD,38 D/DD'40 D/DD,42 D/DD,44 D/DD,46 D/DD,48 D/DD,50 D/DD,52 D/DD,54 D/DD,56 D/DD,58 D/DD,2XL/3XL,4XL/5XL
    brand=susie,taabu,shyle
@@ -327,9 +333,12 @@ def handle_chat_interaction(prompt):
    lingerieset-push-up-level=level-2,level-3
    lingerieset-bra-straps=back-adjustable,detachable,front-adjustable,fully-adjustable,multiway,non-detachable
    price=0-300,1200-1500,1500-1800,300-600,600-900,900-1200
+   Convert the query into attributes for the Lingerie Set category. Match synonyms or contextual words with the listed lingerie set attributes and provide the result in the following format:
+   category: lingerie-set, url: https://www.shyaway.com/lingerie-set-online/?{attribute=value}
+   Example: category: lingerie-set, url: https://www.shyaway.com/lingerie-set-online/?lingerieset-bra-seam=seamless&color-family=skin&size=medium  
     """,
     "sportswear": """
-    category = sportswear
+    category : sportswear
    offers=flat-20%-off
    size=xs,s,m,l,xl,2xl,3xl,XXL,XXXL
    brand=shyaway,shyle,united-classic,van-heusen
@@ -337,13 +346,16 @@ def handle_chat_interaction(prompt):
    sportswear-type=bra,bottoms,tops
    sportswear-feature=racerback,cross-back,leggings,hoodies,seamless,classic-back,crop-top,jackets,shorts,t-back,t-shirt,joggers,skorts,tank
    sportswear-bra-coverage=high-coverage,medium-coverage,low-coverage
-   sportswear-bra-impact_level=low-impact,medium-impact,hi-impact
+   sportswear-bra-impact-level=low-impact,medium-impact,hi-impact
    sportswear-bra-padding=padded,removable-padding
    sportswear-bra-wiring=wirefree
    price=0-300,1200-1500,1500-1800,300-600,600-900,900-1200
+   Convert the query into attributes for the Sportswear category. Match synonyms or contextual words with the listed sportswear attributes and provide the result in the following format:
+   category: sportswear, url: https://www.shyaway.com/sportswear-online/?{attribute=value}
+   Example : category: sportswear, url: https://www.shyaway.com/sportswear-online/?sportswear-bra-impact-level=hi-impact&color-family=blue&size=L&sportswear-type=bra
     """,  
     "sleepwear": """
-    category = sleepwear
+    category : sleepwear
    fabric=bamboo,cotton,cotton-spandex,lace,mesh,modal,nylon-spandex,polyester-spandex,satin,viscose
    nightwear-feature=intimate,loungewear,maternity,winter-sleepwear
    nightwear-type=babydoll-and-chemise,camisole-and-slip,tops,nightwear-sets,sleep-tee,sleepwear-bottoms,nightgowns,tops
@@ -351,9 +363,12 @@ def handle_chat_interaction(prompt):
    size=xs,s,m,l,xl,2xl,3xl,4xl,5xl
    offers=buy-2-for-599,flat-20%-off
    price=0-300,1200-1500,1500-1800,300-600,600-900,900-1200
+   Convert the query into attributes for the Sleepwear category. Match synonyms or contextual words with the listed sleepwear attributes and provide the result in the following format:
+   category: sleepwear, url: https://www.shyaway.com/sleepwear-online/?{attribute=value}
+   example:category: sleepwear, url: https://www.shyaway.com/sleepwear-online/?color-family=pink&size=XL  
     """,
     "shapewear": """
-    category = shapewear
+    category : shapewear
    shapewear-type=Saree,Butt Shaper,Tummy Tucker,Body Shaper,Thigh Shaper,Torso Slimmer,Shaping Panty,Mid-Thigh Shaper,Compression Tights
    size=xs,s,m,l,xl,2xl,3xl,4xl,5xl
    brand=mybra,shyle,united-classic
@@ -361,18 +376,24 @@ def handle_chat_interaction(prompt):
    fabric=nylon-spandex,polycotton-spandex,polyester-spandex,viscose
    offers=flat-20%-off
    price=0-300,1200-1500,1500-1800,300-600,600-900,900-1200
+   Convert the query into attributes for the Shapewear category. Match synonyms or contextual words with the listed shapewear attributes and provide the result in the following format:
+   category: shapewear, url: https://www.shyaway.com/shapewear-online/?{attribute=value}
+   Examples :category: shapewear, url: https://www.shyaway.com/shapewear-online/?shapewear-feature=tummy-tucker&color-family=black  
     """,
     "accessories":"""
-    category = accessories
+    category : accessories
    accessories-type=bra-bag,bra-essentials,hosiery,masks,socks,boob-tape,adhesive-stick-on,silicone-nipple-pad,bra-extender
    brand=shyle
    color-family=grey,black,white,skin,brown,yellow,orange,pink,red,green,blue,purple,prints,multicolor,jacquard
    fabric=cotton-spandex,elastic,eva,lace,mesh,nylon-spandex,polycotton-spandex,polyester-spandex,silicone
    offers=flat-20%-off
    price=0-300,1200-1500,1500-1800,300-600,600-900,900-1200
+   Convert the query into attributes for the Accessories category. Match synonyms or contextual words with the listed accessories attributes and provide the result in the following format:
+   category: accessories, url: https://www.shyaway.com/accessories-online/?{attribute=value}
+   example: category: accessories, url: https://www.shyaway.com/accessories-online/?color-family=skin 
     """,
     "clothing":"""
-    category = clothing
+    category : clothing
    clothing-type=Legwear,Scarves,Shrugs & Jackets,Tops
    size=FZ
    brand=shyle
@@ -380,15 +401,120 @@ def handle_chat_interaction(prompt):
    color-family=black,blue,brown,green,multicolor,orange,pink,prints,red,skin,white,yellow
    offers=flat-10
    price=0-300,1200-1500,1500-1800,300-600,600-900,900-1200
+   Convert the query into attributes for the Accessories category. Match synonyms or contextual words with the listed clothing attributes and provide the result in the following format:
+   category: clothing, url: https://www.shyaway.com/clothing-online/?{attribute=value}
+   example: category: accessories, url: https://www.shyaway.com/clothing-online/?size=FZ 
     """,
-    
+    "All":"""
+        I want answers related to **shyaway.com** alone.
+category : Bra
+   offers=buy-3-for-1199,buy-2-for-1299,flat-20%-off,buy-3-for-899,flat-50%-off,flat-40%-off,new-arrival
+   color-family=Grey,Black,White,Skin,Brown,Yellow,Orange,Pink,Red,Green,Blue,Purple,Prints
+   fabric=Nylon,Viscose-Spandex,Nylon-Polyester-Spandex,Cotton,Cotton-Spandex,Lace,Mesh,Modal,Polyester-Spandex,Polycotton-Spandex,Satin
+   bra-type=Beginners,Bralette,Cami,Everyday,Fashion-Fancy,Minimiser,Push-Up,T-Shirt,Nursing
+   bra-feature=Backless,Bridal,Casual,Designer,Fancy-Back,Front-Open,Hi-Support,Lacework,Longline,Moulded,No-Sag,Plus-Size,Printed,Sexy,Sleep,Transparent
+   bra-coverage=full-coverage,3-4-th-Coverage,Demi-Coverage
+   bra-padding=Non-Padded,Padded,Removable-Padding,Lightly-Padded
+   bra-wiring=wired,wirefree
+   bra-cup-shape=Balconette,Balcony,Full-Cup,Plunge,T-Shirt-Cup
+   bra-push-up-level=level-1,level-2,level-3
+   bra-closure=back-closure,front-closure,slip-on
+   brand=susie,taabu,shyle,shyaway
+   size=28(D,DD,E),30(A,B,C,D,DD,E),32(A,B,C,D,DD,E,F,G,H,I,J),34(A,B,C,D,DD,E,F,G,H,I,J),36(A,B,C,D,DD,E,F,G,H,I,J),38(A,B,C,D,DD,E,F,G,H,I),40(B,C,D,DD,E,F,G,H,I,J),42(B,C,D,E,F,G),44(B,C,D,E,F,G),46(B,C,D),48(B,C,D),50(B,C,D),52(B,C,D)
+   price=0-300,1200-1500,1500-1800,300-600,600-900,900-1200
+category : Panty
+   offers=buy-3-for-499,buy-3-for-599,flat-20%-off,flat-30,new-arrival
+   brand=susie,taabu,shyle,shyaway
+   color-family=Grey,Black,White,Skin,Brown,Yellow,Orange,Pink,Red,Green,Blue,Purple,Prints,multicolor
+   size=2xl,2xl-3xl,3xl,4xl,5xl,l,l-xl,m,s,s-m,xl,xs,xxl
+   fabric=cotton-spandex,disposable,lace,mesh,modal,nylon,nylon-spandex,polycotton-spandex,polyester-spandex,viscose-spandex
+   panty-type=bikini,boy-shorts,cycling-shorts,hipster,period-panty,thong,tummy-tucker
+   panty-feature=bridal,casual,lacework,maternity,no-vpl,plus-size,printed,seamless,sexy,transparent
+   panty-coverage=full-coverage,low-coverage,medium-coverage,no-coverage
+   panty-waist-level=high-waist,low-waist,medium-waist
+   price=0-300,1200-1500,1500-1800,300-600,600-900,900-1200
+
+category : lingerie-set
+   offers=buy-3-for-499,buy-3-for-599,flat-20%-off,flat-30,new-arrival
+   size=38D/XL,40B/XXL,40C/XXL,40D/XXL,32D/S,32 D/DD,34 D/DD,36 D/DD,38 D/DD'40 D/DD,42 D/DD,44 D/DD,46 D/DD,48 D/DD,50 D/DD,52 D/DD,54 D/DD,56 D/DD,58 D/DD,2XL/3XL,4XL/5XL
+   brand=susie,taabu,shyle
+   color-family=Grey,Black,White,Skin,Brown,Yellow,Orange,Pink,Red,Green,Blue,Purple,Prints,multicolor
+   fabric=cotton-spandex,lace,mesh,nylon-spandex
+   lingerieset-type=everyday,fashion-fancy-bra,push-up,t-shirt
+   lingerieset-panty-type=bikini,hipster,thong
+   lingerieset-feature=bridal,casual,hi-support,lacework,moulded,no-sag,printed,sexy,Transparent
+   lingerieset-panty-feature=bridal,transparent,lacework,,casual,sexy,printed
+   lingerieset-bra-closure=back-closure,front-closure,side-closure,slip-on
+   lingerieset-bra-coverage=demi-coverage,full-coverage,3/4th-coverage
+   lingerieset-panty-coverage=full-coverage,medium-coverage,no-coverage,low-coverage
+   lingerieset-bra-padding=padded,non-padded
+   lingerieset-bra-wiring=wired,wirefree
+   lingerieset-panty-waist-level=hi-impact,low-waist,medium-waist
+   lingerieset-bra-seam=seamless,seamed,darted
+   lingerieset-bra-cup-shape=balconette,balcony,full-cup,plunge,t-shirt-cup
+   lingerieset-push-up-level=level-2,level-3
+   lingerieset-bra-straps=back-adjustable,detachable,front-adjustable,fully-adjustable,multiway,non-detachable
+   price=0-300,1200-1500,1500-1800,300-600,600-900,900-1200
+category : sportswear
+   offers=flat-20%-off
+   size=xs,s,m,l,xl,2xl,3xl,XXL,XXXL
+   brand=shyaway,shyle,united-classic,van-heusen
+   color-family=Grey,Black,White,Skin,Brown,Yellow,Orange,Pink,Red,Green,Blue,Purple,Prints,multicolor
+   sportswear-type=bra,bottoms,tops
+   sportswear-feature=racerback,cross-back,leggings,hoodies,seamless,classic-back,crop-top,jackets,shorts,t-back,t-shirt,joggers,skorts,tank
+   sportswear-bra-coverage=high-coverage,medium-coverage,low-coverage
+   sportswear-bra-impact-level=low-impact,medium-impact,hi-impact
+   sportswear-bra-padding=padded,removable-padding
+   sportswear-bra-wiring=wirefree
+   price=0-300,1200-1500,1500-1800,300-600,600-900,900-1200
+category : sleepwear
+   fabric=bamboo,cotton,cotton-spandex,lace,mesh,modal,nylon-spandex,polyester-spandex,satin,viscose
+   nightwear-feature=intimate,loungewear,maternity,winter-sleepwear
+   nightwear-type=babydoll-and-chemise,camisole-and-slip,tops,nightwear-sets,sleep-tee,sleepwear-bottoms,nightgowns,tops
+   color-family=Grey,Black,White,Skin,Brown,Yellow,Orange,Pink,Red,Green,Blue,Purple,Prints,multicolor
+   size=xs,s,m,l,xl,2xl,3xl,4xl,5xl
+   offers=buy-2-for-599,flat-20%-off
+   price=0-300,1200-1500,1500-1800,300-600,600-900,900-1200
+category : shapewear
+   shapewear-type=Saree,Butt Shaper,Tummy Tucker,Body Shaper,Thigh Shaper,Torso Slimmer,Shaping Panty,Mid-Thigh Shaper,Compression Tights
+   size=xs,s,m,l,xl,2xl,3xl,4xl,5xl
+   brand=mybra,shyle,united-classic
+   color-family=grey,black,white,skin,brown,yellow,orange,pink,red,green,blue,purple,prints,multicolor,jacquard
+   fabric=nylon-spandex,polycotton-spandex,polyester-spandex,viscose
+   offers=flat-20%-off
+   price=0-300,1200-1500,1500-1800,300-600,600-900,900-1200
+
+ category : accessories
+   accessories-type=bra-bag,bra-essentials,hosiery,masks,socks,boob-tape,adhesive-stick-on,silicone-nipple-pad,bra-extender
+   brand=shyle
+   color-family=grey,black,white,skin,brown,yellow,orange,pink,red,green,blue,purple,prints,multicolor,jacquard
+   fabric=cotton-spandex,elastic,eva,lace,mesh,nylon-spandex,polycotton-spandex,polyester-spandex,silicone
+   offers=flat-20%-off
+   price=0-300,1200-1500,1500-1800,300-600,600-900,900-1200
+
+category : clothing
+   clothing-type=Legwear,Scarves,Shrugs & Jackets,Tops
+   size=FZ
+   brand=shyle
+   fabric=cotton-spandex,disposable,elastic,eva,lace,mesh,nylon-spandex,polycotton-spandex,polyester-spandex,silicone
+   color-family=black,blue,brown,green,multicolor,orange,pink,prints,red,skin,white,yellow
+   offers=flat-10
+   price=0-300,1200-1500,1500-1800,300-600,600-900,900-1200 
+
+    I have listed the categories, types, and other attributes, and if a question is raised, I need to convert it into attributes and return the result. Find synonyms and understand the word’s context to match it with the closest attribute or category values I provided. The result must strictly match the defined attributes or categories, ensuring it is relevant to the context of the query.
+	•	Output Format:
+If a query is raised, the result should include the category, matched attribute values, and the corresponding URL, structured as follows:
+category: {category.lower()}, url: https://www.shyaway.com/{category.lower()}-online/?{attribute=value}
+	•	Justification:
+Provide justification for your suggestion in the second line. Explain why the matched attribute or value is suitable by considering the type of support, context, or synonym matching, and ensure the selected values suit the question correctly.
+	•	Multiple Values:
+If there is more than one matching attribute value, mention them as comma-separated values in the output.
+Example : category: bra, url: https://www.shyaway.com/bra-online/?color-family=red,green&size=32B  
+"""
+
 }
-#     common=f"""
-# i have provided attributes and attribute values for each category if a question is raised then i need to convert that as attributes and return as output. Find the synonym,understanding the word to match attribute for each word and match with the attribute values and provide the result which should only match with the attribute or category I gave. For example, if i am asking for an apple color bra with 32b size then the result should be like category: {category.lower()},[here]url:https://www.shyaway.com/{category.lower()}-online/?color-family=red,green,size:xl  and if there are more than one attribute value mention that as comma seperated please note that if we have one category then those attribute and attribute values has to be considered like if the question is related to bra then we should consider only those attributes and the common attributes can be used for all categories but dont deviate other than the attribute values i gave
-# """
-    common = f"""i have listed out the category and other attributes and if a question is raised then i need to convert that as attributes and return that as output. Find the synonym,understanding the word to match attribute for each word and match with the attribute values and provide the result which should only match with the attribute or category I gave BY JUSTIFYING YOUR SUGGESTION
-    For example, if i am asking for an apple color with 32b size then the result should be like category: {category.lower()},[here]url:https://www.shyaway.com/{category.lower()}-online/?color-family=red,green,size=xl  one more example like if we are searching for bamboo bra then since we don't have bamboo we can match than with cotton and provide the result CONSIDER THE WHETHER,TYPE OF SUPPORT AND WHICH ATTRIBUTE SUITS THE QUESTIONS CORRECTLY WITH JUSTIFICATION IN SECOND LINE and if there are more than one attribute value mention that as comma seperated"""
-    #  # Get the specific hello_prompt based on the selected_tab
+    common = """If no direct match is found, infer the closest matching attribute based on context and justify your suggestion.List multiple matching attribute values as comma-separated.dont divaite from i gave the prompt should provide requested attribute and value if the user ask size you should provide """
+    # Get the specific hello_prompt based on the selected_tab
     hello_prompt = f"{category_prompts.get(st.session_state.selected_tab, "")}{common}"
 
     print(hello_prompt)
